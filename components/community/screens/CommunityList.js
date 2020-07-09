3b7929f5a99c72b739a3462b5c5c5fe2fe466c20
David Shau <davidshau22@berkeley.edu>
2020-07-08 21:01:02 -0700

    turned animatable list items into a class

--- components/community/screens/CommunityList.js
+++ components/community/screens/CommunityList.js
@@ -14,31 +14,12 @@ const CommunityList = (props) => {
     {
       title: pendingTitle,
       data: pendingList
-      // data: []
     },
     {
       title: peopleTitle,
       data: itemList
-      // data: []
     }
   ]
-  // stores the animation starting point for list items
-  const INIT_OPACITY = 1
-  const [animation, _] = React.useState(new Animated.Value(INIT_OPACITY))
-  // flag to animate only the list items that succeeded
-  const [idxsToAnimate, setIdxsToAnimate] = React.useState([])
-  // animates the disappearance of a user list item
-  const disappear = () => {
-    const disappearAnimations = [
-      Animated.timing(animation, {
-        toValue: 0,
-        duration: DISAPPEAR_TIME,
-        useNativeDriver: true
-      }),
-    ];
-    Animated.sequence(disappearAnimations).start()
-  }
-  console.log("oaiwjdioawd ", idxsToAnimate)
   return (
     <View style={styles.container}>
       <SectionList
@@ -47,14 +28,12 @@ const CommunityList = (props) => {
         // CONSIDER CHANGING THIS TO JUST INCLUDE THE LISTEM. THEN PUT ALL REQUEST FUNCTION
         // STUFF INTO HERE IT'LL BE A PHAT COMPONENT THO
         renderItem={({ item, section, index }) => (
-          <Animated.View
-            key={index}
-            style={{
-              opacity: idxsToAnimate.includes(index) ? animation : INIT_OPACITY
-            }}
-          >
-            {props.listItem({item, section, onItemPress, setAppState, userDataContext, disappear, setIdxsToAnimate, index})}
-          </Animated.View>
+          <props.listItem
+            item={item}
+            section={section}
+            onItemPress={onItemPress}
+            setAppState={setAppState}
+          />
         )}
         renderSectionHeader={({ section: { title } }) => (
           <Text style={styles.header}>{title}</Text>
@@ -69,7 +48,6 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
     justifyContent: 'center',
-    // alignItems: 'center',
   },
   header: {
     paddingTop: 10,
--- components/community/screens/listItems/Follower.js
+++ components/community/screens/listItems/Follower.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Tooltip, Text, ListItem } from 'react-native-elements';
-import { View, StyleSheet, SectionList, Alert } from 'react-native'
+import { View, StyleSheet, SectionList, Alert, Animated } from 'react-native'
 import ActionButton from '../../ActionButton'
 import AcceptRejectButton from './AcceptRejectButton'
 import COMMUNITY_CONSTANTS from '../../CommunityConstants'
 import ENDPOINTS from '../../../endpoints'
+import { UserDataContext } from '../../../../Context'
 import { getData, storeDataObj } from '../../../utils/storage'
 const defaultProfile = require('../../../assets/profile.png')
 const { FOLLOWERS, REQUESTS, DISAPPEAR_TIME } = COMMUNITY_CONSTANTS
@@ -13,13 +14,28 @@ const ACCEPT_FOLLOWER_REQUEST = 'accept follower request'
 const REJECT_FOLLOWER_REQUEST = 'reject follower request'
 const REMOVE_FOLLOWER         = 'remove follower'
 
-const Follower = (props) => {
-  const { item, section, onItemPress, setAppState, disappear, setIdxsToAnimate, index } = props;
-  // this is a work around cuz using useContext here violates some hook rule...
-  const { userDataContext } = props
-
+class Follower extends Component {
+  constructor(props) {
+    super(props)
+    // stores the animation starting point for list items
+    const INIT_OPACITY = 1
+    this.state = {
+      animation: new Animated.Value(INIT_OPACITY),
+    }
+  }
+  disappear() {
+    console.log('disappear: ', this.state.animation)
+    const disappearAnimations = [
+      Animated.timing(this.state.animation, {
+        toValue: 0,
+        duration: DISAPPEAR_TIME,
+        useNativeDriver: true
+      }),
+    ];
+    Animated.sequence(disappearAnimations).start()
+  }
   // generates the error message based on what action the user performed
-  const errorMsg = (action, requester) => {
+  errorMsg(action, requester) {
     switch(action) {
       case ACCEPT_FOLLOWER_REQUEST:
         return `Something went wrong with accepting ${requester.firstName} ${requester.lastName}'s request. Please try again.`
@@ -31,8 +47,8 @@ const Follower = (props) => {
   }
 
   // returns new app state after accepting a follower
-  const accpetFollowerNewState = (requester) => {
-    const newFollowerRequests = userDataContext.followerRequests.filter(user => {
+  accpetFollowerNewState(requester) {
+    const newFollowerRequests = this.context.followerRequests.filter(user => {
       return user._id !== requester._id
     })
     const newFollower = {
@@ -42,30 +58,30 @@ const Follower = (props) => {
       profilePicUrl: requester.profilePicUrl
     }
     return {
-      ...userDataContext,
-      followers: [...userDataContext.followers, newFollower],
+      ...this.context,
+      followers: [...this.context.followers, newFollower],
       followerRequests: newFollowerRequests
     }
   }
 
   // returns new app state after rejecting a follower
-  const rejectFollowerNewState = (requester) => {
-    const newFollowerRequests = userDataContext.followerRequests.filter(user => {
+  rejectFollowerNewState(requester) {
+    const newFollowerRequests = this.context.followerRequests.filter(user => {
       return user._id !== requester._id
     })
     return {
-      ...userDataContext,
+      ...this.context,
       followerRequests: newFollowerRequests
     }
   }
 
   // updates async storage after removing a follower
-  const removeFollowerNewState = (requester) => {
-    const newFollowers = userDataContext.followers.filter(user => {
+  removeFollowerNewState(requester) {
+    const newFollowers = this.context.followers.filter(user => {
       return user._id !== requester._id
     })
     return {
-      ...userDataContext,
+      ...this.context,
       followers: newFollowers
     }
   }
@@ -73,8 +89,10 @@ const Follower = (props) => {
   // will accept/reject follower request or remove the follower 
   // setButtonText is passed as a useState function from action button.
   // it will set the text of the action button once the request is complete
-  const buttonAction = async (requester, action) => {
+  async buttonAction(requester, action) {
     // set button text to be loading
+    const {  setAppState, disappear } = this.props;
+    console.log("action taken: ", action)
     var endPoint;
     switch(action) {
       case ACCEPT_FOLLOWER_REQUEST:
@@ -90,9 +108,9 @@ const Follower = (props) => {
       const userToken = await getData();
       const requestBody = {
         userToken,
-        receiverFirstName: userDataContext.firstName,
-        receiverLastName: userDataContext.lastName,
-        receiverProfilePicUrl: userDataContext.profilePicture.profileUrl,
+        receiverFirstName: this.context.firstName,
+        receiverLastName: this.context.lastName,
+        receiverProfilePicUrl: this.context.profilePicture.profileUrl,
         
         requesterID: requester._id,
         requesterFirstName: requester.firstName,
@@ -113,9 +131,10 @@ const Follower = (props) => {
       console.log(e)
       Alert.alert(
         'Oh No :(',
-        errorMsg(action, requester),
+        this.errorMsg(action, requester),
         [{ text: "Okay" }]
       );
+      return;
     }
     // set the app state and update async storage separately hear, and throw error
     // asking for refresh if this fails
@@ -123,19 +142,18 @@ const Follower = (props) => {
       var newState;
       switch(action) {
         case ACCEPT_FOLLOWER_REQUEST:
-          newState = accpetFollowerNewState(requester);
+          newState = this.accpetFollowerNewState(requester);
           break;
         case REJECT_FOLLOWER_REQUEST:
-          newState = rejectFollowerNewState(requester);
+          newState = this.rejectFollowerNewState(requester);
           break;
         case REMOVE_FOLLOWER:
-          newState = removeFollowerNewState(requester);
+          newState = this.removeFollowerNewState(requester);
       }
       console.log('setting new state: ', newState)
-      // await storeDataObj(newState)
+      await storeDataObj(newState)
       // start the disappearing animation
-      setIdxsToAnimate(prevIdxsToAnimate => [...prevIdxsToAnimate, index])
-      disappear()
+      this.disappear()
       // delay setting app state so diappear animation can complete
       setTimeout(() => {
         console.log("done!")
@@ -150,44 +168,56 @@ const Follower = (props) => {
       );
     }
   }
+  render() {
+    console.log('render: ', this.state.animation)
 
-  return (
-    <ListItem
-      key={item._id}
-      title={`${item.firstName} ${item.lastName}`}
-      // subtitle={section.title === 'Requests' ? 'wants to follow you!' : 'is following you'}
-      // MAKE THIS THEIR PHOTO. USE CLOUDINARY URL
-      leftAvatar={item.profilePicUrl.length > 0 ?
-        { source: { uri: item.profilePicUrl }} : 
-        { source: defaultProfile } 
-      }
+    const { item, section, onItemPress } = this.props;
+    return (
+      <Animated.View
+        style={{
+          opacity: this.state.animation
+        }}
+      >
+        <ListItem
+          key={item._id}
+          title={`${item.firstName} ${item.lastName}`}
+          // subtitle={section.title === 'Requests' ? 'wants to follow you!' : 'is following you'}
+          // MAKE THIS THEIR PHOTO. USE CLOUDINARY URL
+          leftAvatar={item.profilePicUrl.length > 0 ?
+            { source: { uri: item.profilePicUrl }} : 
+            { source: defaultProfile } 
+          }
+    
+          // MAKE THIS THE FOLLOW/ACCEPT BUTTON
+          rightElement={() => {
+            switch(section.title) {
+              case(REQUESTS):
+                return (
+                  <AcceptRejectButton
+                    accept={() => this.buttonAction(item, ACCEPT_FOLLOWER_REQUEST)}
+                    reject={() => this.buttonAction(item, REJECT_FOLLOWER_REQUEST)}
+                  />
+                )
+              case(FOLLOWERS):
+                return (
+                  <ActionButton
+                    initialTitle='Remove'
+                    onPress={() => this.buttonAction(item, REMOVE_FOLLOWER)}
+                  />
+                )
+            }
+          }}
+          bottomDivider
+          onPress={() => {
+            console.log("friend pressed")
+            // get the user's profile and display based on their settings
+            onItemPress(item)
+          }}
+        />
+      </Animated.View>
 
-      // MAKE THIS THE FOLLOW/ACCEPT BUTTON
-      rightElement={() => {
-        switch(section.title) {
-          case(REQUESTS):
-            return (
-              <AcceptRejectButton
-                accept={() => buttonAction(item, ACCEPT_FOLLOWER_REQUEST)}
-                reject={() => buttonAction(item, REJECT_FOLLOWER_REQUEST)}
-              />
-            )
-          case(FOLLOWERS):
-            return (
-              <ActionButton
-                initialTitle='Remove'
-                onPress={() => buttonAction(item, REMOVE_FOLLOWER)}
-              />
-            )
-        }
-      }}
-      bottomDivider
-      onPress={() => {
-        console.log("friend pressed")
-        // get the user's profile and display based on their settings
-        onItemPress(item)
-      }}
-    />
-  )
+    )
+  }
 }
+Follower.contextType = UserDataContext
 export default Follower
