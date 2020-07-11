86decfa2c0810b5d23f30e6b02c5030c5ce3c31c
David Shau <davidshau22@berkeley.edu>
2020-07-10 22:28:00 -0700

    added components

---
+++ components/configure/Config.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Stylesheet } from 'react-native'
+export default function Config(props) {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  
+})
---
+++ components/configure/EditMode.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Stylesheet } from 'react-native'
+export default function EditMode(props) {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  
+})
